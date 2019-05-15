f97835b7de41b270fb875ffdb835576a7d48d5ce
jseidel5 <josuaseidel@gmail.com>
2019-05-16 00:37:47 +0200

    Added function to map values to sequence (not working yet)

--- algorithm.py
+++ algorithm.py
@@ -3,8 +3,10 @@
 
 filename = 'Structure_propensities.txt'
 
-def read_propensities_into_dic(filename, column): # creates dictionary and adds AS (key) + desired colum (value) into it
-    file = open(filename, 'r')
+
+# creates dictionary and adds AS (key) + desired colum (value) into it
+def read_propensities_into_dic(file, column):
+    file = open(file, 'r')
     dic_temp = {}
     word_matrix = []
     word = ''
@@ -14,7 +16,7 @@ def read_propensities_into_dic(filename, column): # creates dictionary and adds 
         else:
             word_list = []
             for char in line:
-                if char != ' ' and char != '\n' and  char != '\r': # \r for linux
+                if char != ' ' and char != '\n' and char != '\r':  # \r for linux
                     word = word + char
                 elif (char == ' ' or char == '\n' or char == '\r') and word != '':
                     # last value missing if it's last line of file. fixed by adding another '--' line.
@@ -27,4 +29,20 @@ def read_propensities_into_dic(filename, column): # creates dictionary and adds 
         dic_temp[element[0]] = float(element[column])
     return dic_temp
 
-print(read_propensities_into_dic(filename,1))
+
+helix_dic = read_propensities_into_dic(filename, 1)
+sheet_dic = read_propensities_into_dic(filename, 2)
+turn_dic = read_propensities_into_dic(filename, 3)
+print('Helix', helix_dic)
+print('Sheet', sheet_dic)
+print('Turn', turn_dic)
+
+
+# creates list of secondary structure values for sequence
+def map_dic_to_seq(sequence, dictionary):
+    value_list = []
+    for key in sequence:
+        value_list.append(dictionary[sequence] = dictionary.values())  # doesn't work yet
+    return value_list
+
+print(map_dic_to_seq('ARD', helix_dic))
